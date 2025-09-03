8abd46eca9cfab6092687df33d84289cfe768f2d
Forestcat <71219154+forest-cat@users.noreply.github.com>
2025-09-04 00:35:42 +0200

    Change string and remove whitespaces

--- usermods/seven_segment_display_reloaded/seven_segment_display_reloaded.cpp
+++ usermods/seven_segment_display_reloaded/seven_segment_display_reloaded.cpp
@@ -600,7 +600,7 @@ const char UsermodSSDR::_str_days[]        PROGMEM = "LED-Numbers-Day";
 const char UsermodSSDR::_str_months[]      PROGMEM = "LED-Numbers-Month";
 const char UsermodSSDR::_str_years[]       PROGMEM = "LED-Numbers-Year";
 const char UsermodSSDR::_str_ldrEnabled[]  PROGMEM = "enable-auto-brightness";
-const char UsermodSSDR::_str_maxLength[]   PROGMEM = "max length of the clock";
+const char UsermodSSDR::_str_maxLength[]   PROGMEM = "max-length-of-the-clock";
 const char UsermodSSDR::_str_minBrightness[]  PROGMEM = "auto-brightness-min";
 const char UsermodSSDR::_str_maxBrightness[]  PROGMEM = "auto-brightness-max";
 
